f003fbbf85593607696f68f41641896912ac9d5c
Mahmoud Azamtta <mahmoud.azamtta19@gmail.com>
2024-04-06 04:14:52 +0300

    Configure telescope to ignore node_modules directories

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,12 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules" },
+        },
+      })
       local opts = { noremap = true, silent = true }
       opts.desc = "Find files"
       vim.keymap.set("n", "<leader>ff", builtin.find_files, opts)

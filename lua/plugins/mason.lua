24eca8a080ff486174e303dbf6379f96a7e10a68
Mahmoud Azamtta <mahmoud.azamtta19@gmail.com>
2024-04-06 04:13:20 +0300

    Delete unnecessary comments

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,13 +2,11 @@ return {
   {
     "williamboman/mason.nvim",
     dependencies = {
-      -- "neovim/nvim-lspconfig",
       "williamboman/mason-lspconfig.nvim",
     },
     config = function()
       local mason = require("mason")
       local mason_lspconfig = require("mason-lspconfig")
-      --   local nvimLSP = require("lspconfig")
       mason.setup({
         ui = {
           icons = {
@@ -31,40 +29,14 @@ return {
           "pyright",
         },
       })
-      --   nvimLSP.lua_ls.setup({})
-      --   nvimLSP.tsserver.setup({})
-      --   nvimLSP.tailwindcss.setup({})
-      --   vim.keymap.set("n", "K", vim.lsp.buf.hover)
-      --   vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      --   vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
-  -- {
-  --   config = function()
-  --     local masonLSP = require("mason-lspconfig")
-  --     masonLSP.setup({
-  --       ensure_installed = { "lua_ls", "tsserver", "tailwindcss" },
-  --     })
-  --   end,
-  -- },
-  --  {
-  --    "neovim/nvim-lspconfig",
-  --    config = function()
-  --      local nvimLSP = require("lspconfig")
-  --      nvimLSP.lua_ls.setup({})
-  --      nvimLSP.tsserver.setup({})
-  --      nvimLSP.tailwindcss.setup({})
-  --      vim.keymap.set("n", "K", vim.lsp.buf.hover)
-  --      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-  --      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-  --    end,
-  --  },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     config = function()
       local mason_tool_installer = require("mason-tool-installer")
       mason_tool_installer.setup({
-        ensure_installed = { "prettier", "stylua", "isort", "black", "pylint", "eslint_d" },
+        ensure_installed = { "prettier", "stylua", "isort", "black", "ruff", "flake8", "eslint_d" },
       })
     end,
   },

ce55b03e31d69696f2c59d40a742af7ff5b56dd5
Mahmoud Azamtta <mahmoud.azamtta19@gmail.com>
2024-04-06 04:09:04 +0300

    Change comment keymaps

--- lua/plugins/comment-nvim.lua
+++ lua/plugins/comment-nvim.lua
@@ -1,32 +1,34 @@
 return {
-  "numToStr/Comment.nvim",
-  opts = {},
-  lazy = false,
-  config = function()
-    local comment = require("Comment")
-    comment.setup({
-      padding = true,
-      sticky = true,
-      ignore = nil,
-      toggler = {
-        line = "gcc",
-        block = "gbc",
-      },
-      opleader = {
-        line = "gc",
-        block = "gb",
-      },
-      extra = {
-        above = "gcO",
-        below = "gco",
-        eol = "gcA",
-      },
-      mappings = {
-        basic = true,
-        extra = true,
-      },
-      pre_hook = nil,
-      post_hook = nil,
-    })
-  end,
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+    lazy = false,
+    config = function()
+      local comment = require("Comment")
+      comment.setup({
+        padding = true,
+        sticky = true,
+        ignore = nil,
+        toggler = {
+          line = "<leader>/",
+          -- block = "<leader>bc",
+        },
+        opleader = {
+          line = "<leader>/",
+          -- block = "<leader>b",
+        },
+        -- extra = {
+        --   above = "<leader>cO",
+        --   below = "<leader>co",
+        --   eol = "<leader>cA",
+        -- },
+        mappings = {
+          basic = true,
+          extra = true,
+        },
+        pre_hook = nil,
+        post_hook = nil,
+      })
+    end,
+  },
 }

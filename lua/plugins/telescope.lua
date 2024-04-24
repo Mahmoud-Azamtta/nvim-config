bb9fcaa2067053e3ce4fa0fe1d571fdfa4bc3f3c
Mahmoud Azamtta <mahmoud.azamtta19@gmail.com>
2024-04-24 23:54:59 +0300

    Update some telescope functionalities

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,16 +15,12 @@ return {
       local opts = { noremap = true, silent = true }
       opts.desc = "Find files"
       vim.keymap.set("n", "<leader>ff", builtin.find_files, opts)
-      -- vim.keymap.set("n", "<leader>ff", function()
-      --   builtin.find_files({
-      --     prompt_title = "Find Files",
-      --     cwd = "C:\\Users\\m7mod\\Github",
-      --   })
-      -- end, opts)
       opts.desc = "Live grep"
       vim.keymap.set("n", "<leader>fl", builtin.live_grep, opts)
       opts.desc = "Git files"
       vim.keymap.set("n", "<leader>fg", builtin.git_files, opts)
+      opts.desc = "Find string under cursor"
+      vim.keymap.set("n", "<leader>fc", builtin.grep_string, opts)
     end,
   },
   {
